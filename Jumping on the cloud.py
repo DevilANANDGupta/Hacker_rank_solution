d9b6e1481670caefabcf31cfb7472fa467956036
anand gupta <90339262+DevilANANDGupta@users.noreply.github.com>
2022-05-16 04:18:42 +0530

    Create Jumping on the cloud.py

---
+++ Jumping on the cloud.py
@@ -0,0 +1,103 @@
+'''There is a new mobile game that starts with consecutively numbered clouds. Some of the clouds are thunderheads and others are cumulus. The player can jump on any cumulus cloud having a number that is equal to the number of the current cloud plus  or . The player must avoid the thunderheads. Determine the minimum number of jumps it will take to jump from the starting postion to the last cloud. It is always possible to win the game.
+
+For each game, you will get an array of clouds numbered  if they are safe or  if they must be avoided.
+
+Example
+
+Index the array from . The number on each cloud is its index in the list so the player must avoid the clouds at indices  and . They could follow these two paths:  or . The first path takes  jumps while the second takes . Return .
+
+Function Description
+
+Complete the jumpingOnClouds function in the editor below.
+
+jumpingOnClouds has the following parameter(s):
+
+int c[n]: an array of binary integers
+Returns
+
+int: the minimum number of jumps required
+Input Format
+
+The first line contains an integer , the total number of clouds. The second line contains  space-separated binary integers describing clouds  where .
+
+Constraints
+
+Output Format
+
+Print the minimum number of jumps needed to win the game.
+
+Sample Input 0
+
+7
+0 0 1 0 0 1 0
+Sample Output 0
+
+4
+Explanation 0:
+The player must avoid  and . The game can be won with a minimum of  jumps:
+
+jump(2).png
+
+Sample Input 1
+
+6
+0 0 0 0 1 0
+Sample Output 1
+
+3
+Explanation 1:
+The only thundercloud to avoid is . The game can be won in  jumps:
+
+jump(5).png'''
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'jumpingOnClouds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY c as parameter.
+#
+
+def jumpingOnClouds(c):
+    # Write your code here
+    current_position = 0
+    number_of_jumps = 0
+    last_cloud_postion = len(c)-1
+    last_second_postion = len(c)-2
+    
+    while current_position<last_second_postion:
+        #Checking if the cloud next to the next cloud is thunderstorm
+        if c[current_position+2] == 0:
+            current_position += 2
+        else:
+            current_position += 1
+        number_of_jumps += 1
+    #Checking if we are in the last cloud or the last second cloud
+    if current_position != last_cloud_postion:
+        number_of_jumps += 1
+        
+    return number_of_jumps
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    memo = {}
+
+    n = int(input().strip())
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
+
